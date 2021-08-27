eebabd0cd6f0caf95fe58cf359bd3503e0460893
Julia Crooijmans <juliacrooijmans@gmail.com>
2021-08-27 11:26:48 -0400

    porfolio

--- server/db.js
+++ server/db.js
@@ -1,10 +1,24 @@
 const Sequelize = require('sequelize')
+const databaseName = 'portfolio'
+
+
+const config = {
+    logging: false
+};
+
+if (process.env.LOGGING === 'true') {
+    delete config.logging
+}
+
+//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+if (process.env.DATABASE_URL) {
+    config.dialectOptions = {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    };
+}
 
 const db = new Sequelize(
-    process.env.DATABASE_URL || 'postgres://localhost:5432/portfolio',
-    {
-        logging: false,
-    }
-)
-
+    process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
 module.exports = db
