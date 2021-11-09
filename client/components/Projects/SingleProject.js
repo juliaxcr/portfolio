4fccfc4f83bd2befb224ad204b7d4a89b293847d
Julia Crooijmans <84352176+juliaxcr@users.noreply.github.com>
2021-11-09 10:42:54 -0500

    heroku

--- client/components/Projects/SingleProject.js
+++ client/components/Projects/SingleProject.js
@@ -11,40 +11,47 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 class SingleProject extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            project: {}
+        }
     }
 
-    componentWillMount() {
+    componentDidMount() {
         this.props.fetchAllProjects()
+        const project = this.props.projects.filter(project => (
+            project.name === this.props.projectName
+        ))[0]
+        this.setState({ project })
     }
 
     render() {
         // problematic code
         if (this.props.projectsLoading) return <div id="spinner"><Loader type="TailSpin" color="#294c60" height={50} width={50} /></div>
-        else {
-            const project = this.props.projects.filter(project => (
-                project.name === this.props.projectName
-            ))[0]
-        }
+        // else {
+        //     const project = this.props.projects.filter(project => (
+        //         project.name === this.props.projectName
+        //     ))[0]
+        // }
         return (
             <div id="single-project">
                 <div id="single-project-top">
                     <div id="folder-logo">
-                        <Link to={`/projects/${project.name}`}>
+                        <Link to={`/projects/${this.state.project.name}`}>
                             <FontAwesomeIcon icon={faFolderOpen} className="single-proj-fa" />
                         </Link>
                     </div>
                     <div id="social-media-logo">
-                        <a href={project.github} target="_blank">
+                        <a href={this.state.project.github} target="_blank">
                             <FontAwesomeIcon icon={faGithub} className="single-proj-smaller-fa" />
                         </a>
-                        <a href={project.heroku} target="_blank">
+                        <a href={this.state.project.heroku} target="_blank">
                             <FontAwesomeIcon icon={faExpandAlt} className="single-proj-smaller-fa" />
                         </a>
                     </div>
                 </div>
                 <div id="single-project-content">
-                    <h4>{project.name}</h4>
-                    <p>{project.shortContent}</p>
+                    <h4>{this.state.project.name}</h4>
+                    <p>{this.state.project.shortContent}</p>
                 </div>
             </div>
         )
